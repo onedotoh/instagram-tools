bace572cff553d0013b2bec58602a382386f8547
Mahmoud Safar <mahmoudsafar96@gmail.com>
2020-04-01 23:12:56 +0300

    Finished hashtag randomizer

--- hashtag-randomizer/src/app/app.module.ts
+++ hashtag-randomizer/src/app/app.module.ts
@@ -4,6 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ManagerComponent } from './page/manager/manager.component';
+import { NgZorroAntdModule, NZ_I18N, en_US, NzInputModule } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd';
+
+
+registerLocaleData(en);
+const ngZorroConfig: NzConfig = {
+  message: { nzMaxStack: 3 }
+};
 
 @NgModule({
   declarations: [
@@ -12,9 +28,17 @@ import { ManagerComponent } from './page/manager/manager.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzInputModule,
+    NzGridModule,
+    NzButtonModule,
+    NzMessageModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }, { provide: NZ_CONFIG, useValue: ngZorroConfig }],
   bootstrap: [AppComponent],
   exports: [ManagerComponent]
 })
--- hashtag-randomizer/src/app/page/manager/manager.component.ts
+++ hashtag-randomizer/src/app/page/manager/manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-manager',
@@ -7,10 +8,82 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ManagerComponent implements OnInit {
+  high: string;
+  mid: string;
+  low: string;
+  brand: string;
 
-  constructor() { }
+  result: string;
+  size: number;
+
+  constructor(private message: NzMessageService) { }
 
   ngOnInit() {
+    this.high = '#mdcommunity #zbrush #thegraphicspr0ject #surreal42 #rsa_graphics #c4d #cinema4d #graphicdesign #behance #dribbble #graphicgang #adobe #photoshop #adobephotoshop #adobeillustrator #render #creative #abstract #inspiration #3dsmax #cgi #3dmodel #3dart #3d #3d #cgi #render #adobephotoshop #thegraphicspr0ject #creative #cinema4d #3dsmax #zbrush #adobe';
+    this.mid = '#dailyrender #renderzone #c4dart #octanerender #otoy #xuxoe #mgcollective #redshift #b3d #blender3d #renderzone #cgsociety #autodeskmaya #fa_hypnotic #d_expo #howiseedatworld #gsgdaily #mograph #dribbblers #gfxmob #eyeondesign #designfeed';
+    this.low = '#blendercommunity #blenderrender #cgiart #digitalsculpting #3dsculpting #zbrushart #zbrushsculpt #thedesigntalks #blender28 #digital3d #zbrushcentral';
+    this.brand = '#polyspheric';
+    this.initResult();
+  }
+
+  initResult() {
+    this.result = '';
+    // this.result += (this.shuffle(this.high.split(' ')).slice(0, 10).toString().replace(/,/g, ' '));
+    // this.result += ' ' + (this.shuffle(this.mid.split(' ')).slice(0, 10).toString().replace(/,/g, ' '));
+    // this.result += ' ' + (this.shuffle(this.high.split(' ')).slice(0, 10).toString().replace(/,/g, ' '));
+
+    let arr = [];
+
+    arr = arr.concat(this.shuffle(this.high.split(' ')).slice(0, 10));
+    arr = arr.concat(this.shuffle(this.mid.split(' ')).slice(0, 10));
+    arr = arr.concat(this.shuffle(this.low.split(' ')).slice(0, 10));
+
+    arr = this.shuffle(arr);
+
+    arr[0] = this.brand;
+
+    this.size = arr.length;
+
+    this.result = arr.toString().replace(/,/g, ' ');
+
+
+  }
+
+
+  shuffle(array): [] {
+    // tslint:disable-next-line: one-variable-per-declaration
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  copyToClipboard() {
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.result;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+    this.initResult();
+    this.message.info('Copied to Clipboard!');
   }
 
 }
